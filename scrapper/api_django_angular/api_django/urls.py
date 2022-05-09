738feb5a16a8ccc1f21cfc3ec2a003eaff03dc3d
Coumba Camara <coumba.camara75@gmail.com>
2022-05-09 03:27:59 +0200

    re-install django api and fix bugs

---
+++ scrapper/api_django_angular/api_django/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.views.generic.base import TemplateView
+from api_django.views import *
+from django.urls import re_path
+
+urlpatterns = [
+   re_path(r'^admin/', admin.site.urls),
+   re_path(r'^getData/', get_data),
+   re_path(r'^.*', TemplateView.as_view(template_name="home.html"), name="home")
+]

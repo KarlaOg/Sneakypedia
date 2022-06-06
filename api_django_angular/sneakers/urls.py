5c40a89db043be4485caa8e8299f88c8608b2400
KarlaOg <karla.og@supinternet.fr>
2022-06-07 00:23:15 +0200

    Test heroku for API

--- api_django_angular/sneakers/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from api_django.views import JsonView
-
-urlpatterns = [
-    path('sneakers/',JsonView.as_view()),
-]
--- pwa/src/app/models/sneaker.ts
+++
@@ -1,6 +0,0 @@
-export interface Sneaker {
-  label?: string,
-  image: string,
-  description?: string,
-  price: number,
-}

6916e9e5401c59ec41ed0151df28d87a63337527
KarlaOg <karla.og@supinternet.fr>
2022-06-14 22:13:33 +0200

    [WIP]

--- pwa/src/app/components/login/login.component.ts
+++ pwa/src/app/components/login/login.component.ts
@@ -7,7 +7,7 @@ import { UserService } from '../../services/user/user.service';
 @Component({
   selector: 'app-login',
   template: `  
-<div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 ">
+<div class="min-h-full flex items-center  justify-center py-12 px-4 sm:px-6 lg:px-8 ">
   <div class="max-w-md w-full space-y-8">
     <div>
       <h2 class="mt-6 text-center text-xl font-extrabold text-gray-900">CONNECTEZ-VOUS À VOTRE COMPTE</h2>
@@ -39,9 +39,9 @@ import { UserService } from '../../services/user/user.service';
           <label for="remember-me" class="ml-2 block text-sm text-gray-900"> Se souvenir </label> -->
         </div>
 
-        <div class="text-sm">
+        <!-- <div class="text-sm">
           <a href="#" class="font-medium text-black hover:text-orange-400  underline underline-offset-4 "> Mot de passe oublié</a>
-        </div>
+        </div> -->
       </div>
 
       <div>

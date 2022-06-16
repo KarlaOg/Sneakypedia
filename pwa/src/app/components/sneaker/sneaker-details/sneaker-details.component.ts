6d6b40b4be4ddef81ad693e31b34b70cdb28145d
Coumba Camara <coumba.camara75@gmail.com>
2022-06-16 13:24:38 +0200

    wip

--- pwa/src/app/components/sneaker/sneaker-details/sneaker-details.component.ts
+++ pwa/src/app/components/sneaker/sneaker-details/sneaker-details.component.ts
@@ -10,7 +10,7 @@ import { SneakerService } from 'src/app/services/sneaker/sneaker.service';
   template: `
     <div class="bg-white pt-16">
       <div>
-        <div class="max-w-2xl mx-auto pt-10 pb-16 px-4 sm:px-6 lg:max-w-7xl lg:pt-16 lg:pb-24 lg:px-8 lg:grid lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8">
+        <div id="wrapper-content" class="max-w-2xl mx-auto pt-10 pb-16 px-4 sm:px-6 lg:max-w-7xl lg:pt-16 lg:pb-24 lg:px-8 lg:grid lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8">
           <div class="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
             <h1 class="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">
               {{ detailsItem.label }}
@@ -38,11 +38,11 @@ import { SneakerService } from 'src/app/services/sneaker/sneaker.service';
             </button>
           </div>
 
-          <div class=" py-0lg:pt-6 lg:pb-16 lg:col-start-1 lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+          <div class="py-0lg:pt-6 lg:pb-16 lg:col-start-1 lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
 
             <div class="mt-6 max-w-2xl mx-auto lg:max-w-7xl lg:gap-x-8 py-6">
               <div
-                class="hidden aspect-w-3 aspect-h-4 overflow-hidden lg:block"
+                class="aspect-w-3 aspect-h-4 overflow-hidden lg:block"
               >
                 <img
                   src="{{ detailsItem.image }}"

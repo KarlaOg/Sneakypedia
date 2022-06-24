b3192a0e66ee5428813c829371358b2af8752c6a
Coumba Camara <coumba.camara75@gmail.com>
2022-06-24 17:34:47 +0200

    fixes

--- api_django_angular/setup.py
+++ api_django_angular/setup.py
@@ -4,4 +4,4 @@ from setuptools import setup, find_packages
 setup(name='api_django',
       version='1.0',
       packages=find_packages(),
-       scripts=['Pipefile'])
+       scripts=['manage.py'])

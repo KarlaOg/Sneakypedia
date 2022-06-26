cd162234c330160827bab0d24e3525d22d41fc7f
Coumba Camara <coumba.camara75@gmail.com>
2022-06-26 20:45:31 +0200

    gunicorn is the problem?

--- api_django/settings.py
+++ api_django/settings.py
@@ -44,6 +44,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [

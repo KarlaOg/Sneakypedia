fb336bc312ebc4f2f5cac8c35c78b78b50598cf6
Coumba Camara <coumba.camara75@gmail.com>
2022-07-04 05:45:33 +0200

    [WIP] fix angular search results but json response missed sneakers name model

--- api_django_angular/api_django/main/urls.py
+++ api_django_angular/api_django/main/urls.py
@@ -4,5 +4,5 @@ from sneakers.views import JsonView, SearchResults
 urlpatterns = [
     path('sneakers/',JsonView.as_view()),
     path('sneakers/<int:pk>/', JsonView.as_view(), name='sneaker'),
-    path('search/',SearchResults.as_view(), name="search-list")
+    path('search/',SearchResults.as_view())
 ]

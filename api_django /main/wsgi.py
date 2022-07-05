7818b02b1037234108de941de42ae75981fbd780
Coumba Camara <coumba.camara75@gmail.com>
2022-07-05 22:24:06 +0200

    wsgi

--- api_django /main/wsgi.py
+++ api_django /main/wsgi.py
@@ -23,7 +23,7 @@ from django.core.wsgi import get_wsgi_application
 # Add this file path to sys.path in order to import settings
 sys.path.insert(0, os.path.normpath(os.path.join(
     os.path.dirname(os.path.realpath(__file__)), '../..')))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'main.settings'
 sys.stdout = sys.stderr
 
 application = get_wsgi_application()

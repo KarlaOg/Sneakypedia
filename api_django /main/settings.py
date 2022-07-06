29b12ad9830266a047b8b690f4ee8f8a44fc8f83
Coumba Camara <coumba.camara75@gmail.com>
2022-07-06 15:58:50 +0000

    final setup

--- api_django /main/settings.py
+++ api_django /main/settings.py
@@ -73,14 +73,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
-    },
-    'users':{
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env("DATABASE_NAME"),
-        'USER': env("DATABASE_USER"),
-        'PASSWORD': env("DATABASE_PASSWORD"),
-        'HOST': env("DATABASE_HOST"),
-        'PORT': env("DATABASE_PORT"),
     }
 }
 

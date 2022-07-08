91380f759ce1ed180a169dde621e131f21f2235e
KarlaOg <karla.og@supinternet.fr>
2022-07-08 05:59:36 +0200

    FIX Bug

--- pwa/src/app/components/sneaker/sneakers-list/sneakers-list.component.ts
+++ pwa/src/app/components/sneaker/sneakers-list/sneakers-list.component.ts
@@ -8,7 +8,7 @@ import { SneakerService } from 'src/app/services/sneaker/sneaker.service';
   <div class="bg-white">
     <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
 
-       <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8" *ngIf="filteredSneakers.length == 0;else FilteredSneakers">
+       <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8" *ngIf="filteredSneakers.length == 0; else FilteredSneakers">
        <a [routerLink]="['../sneakers', item.id]" class="group" *ngFor="let item of filteredSneakers" >
           <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
             <img src='{{item.image}}' alt="Images of different sneakers" class="w-full h-full object-center object-cover group-hover:opacity-75">
@@ -28,17 +28,6 @@ import { SneakerService } from 'src/app/services/sneaker/sneaker.service';
       </div>
       </ng-template>
 
-      <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-        <a [routerLink]="['../sneakers', item.id]" class="group" *ngFor="let item of filteredSneakers" >
-          <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-            <img src='{{item.image}}' alt="Images of different sneakers" class="w-full h-full object-center object-cover group-hover:opacity-75">
-          </div>
-          <h3 class="mt- 4 text-sm text-gray-700"> {{item.label}}</h3>
-        </a>
-      </div>
-
-
- 
 
   `,
   styleUrls: ['./sneakers-list.component.css']

a1c84269a1545f740817c2d354ce798f42a0e705
KarlaOg <karla.og@supinternet.fr>
2022-07-08 05:15:31 +0200

    FIX

--- pwa/src/app/components/sneaker/sneakers-list/sneakers-list.component.ts
+++ pwa/src/app/components/sneaker/sneakers-list/sneakers-list.component.ts
@@ -5,9 +5,8 @@ import { SneakerService } from 'src/app/services/sneaker/sneaker.service';
 @Component({
   selector: 'app-sneakers-list',
   template: `
-<div class="bg-white">
+  <div class="bg-white">
     <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-      <h2 class="sr-only">Products</h2>
 
        <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8" *ngIf="filteredSneakers.length == 0;else FilteredSneakers">
        <a [routerLink]="['../sneakers', item.id]" class="group" *ngFor="let item of filteredSneakers" >
@@ -19,6 +18,7 @@ import { SneakerService } from 'src/app/services/sneaker/sneaker.service';
       </div>
 
       <ng-template  #FilteredSneakers>
+      </ng-template>
       <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
         <a [routerLink]="['../sneakers', item.id]" class="group" *ngFor="let item of filteredSneakers" >
           <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
@@ -26,8 +26,8 @@ import { SneakerService } from 'src/app/services/sneaker/sneaker.service';
           </div>
           <h3 class="mt- 4 text-sm text-gray-700"> {{item.label}}</h3>
         </a>
-    </div>
-    </ng-template>
+      </div>
+
 
  
 
